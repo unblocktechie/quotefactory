b52ef1bdf8d6fd84a0eece0e353efcb4eb9f9ca5
unknown <unblocktechie@gmail.com>
2021-01-06 15:35:07 +0530

    Updates

--- precache-manifest.c43efe664dc8bcdece17bd3212e6f77c.js
+++ precache-manifest.c43efe664dc8bcdece17bd3212e6f77c.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "434a4a94747abb1254c3",
-    "url": "/quotefactory/static/js/main.434a4a94.chunk.js"
+    "revision": "bd87476197b7153d7fb8",
+    "url": "/quotefactory/static/js/main.bd874761.chunk.js"
   },
   {
     "revision": "82fc285962a88bd24ff8",
@@ -16,60 +16,60 @@ self.__precacheManifest = [
     "url": "/quotefactory/static/js/runtime~main.395792c0.js"
   },
   {
-    "revision": "0ab54153eeeca0ce03978cc463b257f7",
-    "url": "/quotefactory/static/media/icons.0ab54153.woff2"
-  },
-  {
     "revision": "9c74e172f87984c48ddf5c8108cabe67",
     "url": "/quotefactory/static/media/flags.9c74e172.png"
-  },
-  {
-    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
-    "url": "/quotefactory/static/media/icons.b87b9ba5.ttf"
   },
   {
     "revision": "faff92145777a3cbaf8e7367b4807987",
     "url": "/quotefactory/static/media/icons.faff9214.woff"
   },
   {
-    "revision": "701ae6abd4719e9c2ada3535a497b341",
-    "url": "/quotefactory/static/media/outline-icons.701ae6ab.eot"
-  },
-  {
-    "revision": "962a1bf31c081691065fe333d9fa8105",
-    "url": "/quotefactory/static/media/icons.962a1bf3.svg"
-  },
-  {
-    "revision": "cd6c777f1945164224dee082abaea03a",
-    "url": "/quotefactory/static/media/outline-icons.cd6c777f.woff2"
-  },
-  {
     "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
     "url": "/quotefactory/static/media/icons.8e3c7f55.eot"
   },
   {
-    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
-    "url": "/quotefactory/static/media/outline-icons.ad97afd3.ttf"
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/quotefactory/static/media/icons.0ab54153.woff2"
   },
   {
-    "revision": "e8c322de9658cbeb8a774b6624167c2c",
-    "url": "/quotefactory/static/media/brand-icons.e8c322de.woff2"
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/quotefactory/static/media/icons.b87b9ba5.ttf"
   },
   {
-    "revision": "13db00b7a34fee4d819ab7f9838cc428",
-    "url": "/quotefactory/static/media/brand-icons.13db00b7.eot"
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/quotefactory/static/media/outline-icons.701ae6ab.eot"
   },
   {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
     "url": "/quotefactory/static/media/outline-icons.ef60a4f6.woff"
   },
   {
-    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
-    "url": "/quotefactory/static/media/outline-icons.82f60bd0.svg"
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/quotefactory/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/quotefactory/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/quotefactory/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/quotefactory/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/quotefactory/static/media/brand-icons.e8c322de.woff2"
   },
   {
     "revision": "a046592bac8f2fd96e994733faf3858c",
     "url": "/quotefactory/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/quotefactory/static/media/outline-icons.82f60bd0.svg"
   },
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
@@ -80,7 +80,7 @@ self.__precacheManifest = [
     "url": "/quotefactory/static/media/brand-icons.a1a749e8.svg"
   },
   {
-    "revision": "95af74375a6bfbdc3f880ba699b85142",
+    "revision": "22ab3acb6c0ce0bf6a4ac967f7df10ad",
     "url": "/quotefactory/index.html"
   }
 ];
